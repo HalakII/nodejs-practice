39bf151c3320ff66ace64d4c7a8ba5244efdec96
Iryna <1017imiia@gmail.com>
2024-03-03 22:53:12 +0200

    created helpers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const error = require("./src/helpers/error.js");
 
 const app = express();
 const formatLoger = app.get("env") === "dev" ? "dev" : "short";
@@ -10,9 +11,27 @@ app.use(logger(formatLoger));
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("request done?");
-  res.json("hello world");
+app.get("/", (req, res, next) => {
+  const myError = error(401, "My error 1");
+  next(myError);
+  return;
+  res.json("Hello world");
+});
+
+app.get("/flower", (req, res, next) => {
+  const myError = error(402, "My error 2");
+  next(myError);
+  return;
+  res.json("Love flowers");
+});
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((error, _, res, __) => {
+  const { status = 500, message = "Server internal error" } = error;
+  res.status(status).json({ message });
 });
 
 app.listen(process.env.PORT, () => {
