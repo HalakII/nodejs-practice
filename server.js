830baff5fb4ef9b7c74f5372541d0251e5946a42
Iryna <1017imiia@gmail.com>
2024-03-03 22:06:46 +0200

    env file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+const formatLoger = app.get("env") === "dev" ? "dev" : "short";
 
-app.use(logger("dev"));
+app.use(logger(formatLoger));
 app.use(cors());
 app.use(express.json());
 
@@ -13,6 +15,12 @@ app.get("/", (req, res) => {
   res.json("hello world");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });
+
+// Встановлюємо значення порту для додатку
+// app.set("port", process.env.PORT);
+// app.listen(app.get("port"), () => {
+//   console.log("Server is running");
+// });
